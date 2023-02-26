41559adc5954d04e52690450bab49d3e27ebc12a
Sam <iaiznura@gmail.com>
2023-02-26 18:46:50 +0300

    halfdone homework 2

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from utils.channel import Channel
 
 
-channel_id = 'UCSJ4gkVC6NrvII8umztf0Ow'
+# channel_id = 'UCSJ4gkVC6NrvII8umztf0Ow'
+#
+# lofi_girl = Channel(channel_id)
+# # lofi_girl.print_info()
+# print(lofi_girl)
 
-lofi_girl = Channel(channel_id)
-lofi_girl.print_info()
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+
+# получаем значения атрибутов
+print(vdud.title)
+# вДудь
+print(vdud.video_count)
+# 163
+print(vdud.url)
+# https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
+
+# менять не можем
+# vdud.id = 'Новое название'
+# AttributeError: property 'channel_id' of 'Channel' object has no setter
+
+# можем получить объект для работы с API вне класса
+print(Channel.get_service())
+# <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+
+
+# создать файл 'vdud.json' в данными по каналу
+vdud.save_json_in_file('vdud.json')
+
+
+print(vdud.print_info())
--- utils/channel.py
+++ utils/channel.py
@@ -1,18 +1,49 @@
 import os
-
+import json
 from googleapiclient.discovery import build
-from pprint import pprint
 
 
 class Channel:
     # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
     api_key: str = os.getenv('YT_API_KEY')
-    # создать специальный объект для работы с API
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, channel_id: str):
-        self.id = channel_id
+    def __init__(self, id: str):
+        self.__id = id
+        self.json = ""
+        self.to_json()
+        self.title = self.json['items'][0]['snippet']['title']
+        self.chanel_description = self.json["items"][0]['snippet']['description']
+        self.url = "https://www.youtube.com/channel/" + self.__id
+        self.video_count = self.json["items"][0]["statistics"]["videoCount"]
+        self.channel_number_of_views = self.json["items"][0]["statistics"]["viewCount"]
+
+    @property
+    def id(self):
+        return self.__id
 
     def print_info(self):
-        channel = Channel.youtube.channels().list(id=self.id, part='snippet,statistics').execute()
-        pprint(channel)
+        """Вывод информации о канале"""
+        print(self.json)
+
+    def save_json_in_file(self, path):
+        """Сохранение всех атрибутов объекта channel, кроме json, в файл по адресу path"""
+        json_text = {}
+        for i in self.__dict__:
+            if i != 'json':
+                json_text[i] = self.__dict__[i]
+        json_text = str(json_text).replace(',', ',\n')
+        json_text = '[' + json_text + '] \n'
+        # json_text = json.dumps(json_text, indent=4)
+        with open(path, "w", encoding="UTF-8") as file:
+            file.write(json_text + '\n')
+
+    def to_json(self):
+        """Создание объекта для работы с API YouTube"""
+        channel = self.get_service().channels().list(id=self.__id, part='snippet,statistics').execute()
+        self.json = channel
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube"""
+        with build('youtube', 'v3', developerKey=cls.api_key) as youtube:
+            return youtube
