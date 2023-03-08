2a248cfead556f87867b53ef30c5c70c5c3e1bd9
Sam <iaiznura@gmail.com>
2023-03-08 11:43:12 +0300

    done homework 6

--- main.py
+++ main.py
@@ -2,75 +2,91 @@
 from utils.video import Video
 from utils.plvideo import PLVideo
 from utils.playlist import PlayList
+
+
 # import os
 # from googleapiclient.discovery import build
 # from pprint import pprint
 
-# lofi_girl = Channel('UCSJ4gkVC6NrvII8umztf0Ow')
-# # lofi_girl.print_info()
-# print(lofi_girl)
+def main():
+    # lofi_girl = Channel('UCSJ4gkVC6NrvII8umztf0Ow')
+    # # lofi_girl.print_info()
+    # print(lofi_girl)
 
-# vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    # vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 
-# получаем значения атрибутов
-# print(vdud.title)
-# # вДудь
-# print(vdud.number_of_videos)
-# # 163
-# print(vdud.url_link)
-# https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
+    # получаем значения атрибутов
+    # print(vdud.title)
+    # # вДудь
+    # print(vdud.number_of_videos)
+    # # 163
+    # print(vdud.url_link)
+    # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
 
-# менять не можем
-# vdud.id = 'Новое название'
-# AttributeError: property 'channel_id' of 'Channel' object has no setter
+    # менять не можем
+    # vdud.id = 'Новое название'
+    # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
-# # можем получить объект для работы с API вне класса
-# print(Channel.get_service())
-# # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-#
-#
-# # создать файл 'vdud.json' в данными по каналу
-# vdud.save_to_json_file('vdud.json')
+    # # можем получить объект для работы с API вне класса
+    # print(Channel.get_service())
+    # # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+    #
+    #
+    # # создать файл 'vdud.json' в данными по каналу
+    # vdud.save_to_json_file('vdud.json')
 
-# Homework 3
-# _________________________________________________________
-# print(vdud)
-#
-# print(vdud < lofi_girl)
-# print(vdud > lofi_girl)
-# print(lofi_girl + vdud)
+    # Homework 3
+    # _________________________________________________________
+    # # print(vdud)
+    # #
+    # # print(vdud < lofi_girl)
+    # # print(vdud > lofi_girl)
+    # # print(lofi_girl + vdud)
+    #
+    # id = '9lO06Zxhu88'
+    # video1 = Video(id)
+    # # print(video1)
+    # pl_id = "PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD"
 
-id = '9lO06Zxhu88'
-video1 = Video(id)
-# print(video1)
-pl_id = "PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD"
+    # Homework 4
+    # _________________________________________________________
+    # print(video1)
+    #
+    # video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+    # # pprint(video2.pl_info)
+    # print(video2)
+    # # pprint(video2.video_info)
 
-# Homework 4
-# _________________________________________________________
-# print(video1)
+    # Homework 5
+    # _________________________________________________________
+    # pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+    #
+    # print(pl.title)
+    # # Редакция. АнтиТревел
+    #
+    # print(pl.url_link)
+    # # https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb
+    #
+    # duration = pl.total_duration
+    # print(duration)
+    # # 3:41:01
+    # print(type(duration))
+    # # <class 'datetime.timedelta'>
+    # print(duration.total_seconds())
+    # # 13261.0
+    #
+    # pl.show_best_video()
+    # # https://youtu.be/9Bv2zltQKQA
 
-video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-# pprint(video2.pl_info)
-print(video2)
-# pprint(video2.video_info)
+    # Homework 6
+    # _________________________________________________________
+    video_6 = Video('broken_video_id')
+    # video_6 = Video('9lO06Zxhu88')
 
-# Homework 5
-# _________________________________________________________
-pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+    print(video_6.number_of_video_views)
+    print(video_6.title)
+    print(video_6.video_id)
 
-print(pl.title)
-# Редакция. АнтиТревел
 
-print(pl.url_link)
-# https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb
-
-duration = pl.total_duration
-print(duration)
-# 3:41:01
-print(type(duration))
-# <class 'datetime.timedelta'>
-print(duration.total_seconds())
-# 13261.0
-
-pl.show_best_video()
-# https://youtu.be/9Bv2zltQKQA
+if __name__ == '__main__':
+    main()
--- utils/video.py
+++ utils/video.py
@@ -3,12 +3,21 @@ from utils.channel import Channel
 
 class Video:
     def __init__(self, video_id: str) -> None:
+        """Инициализатор. Если пользователь передал id, с которым невозможно получить данные о видео по API,
+         то у экземпляра инициализируется только свойство video_id, а остальные поля принимают значение None."""
         self.video_id = video_id
-        self.video_info = Channel.get_service().videos().list(id=self.video_id, part='snippet,statistics').execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.number_of_video_views = self.video_info['items'][0]['statistics']['viewCount']
-        self.number_of_likes = self.video_info['items'][0]["statistics"]['likeCount']
-        self.url_link = "https://www.youtube.com/watch?v=" + self.video_id
+        try:
+            self.video_info = Channel.get_service().videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+            self.number_of_video_views = self.video_info['items'][0]['statistics']['viewCount']
+            self.number_of_likes = self.video_info['items'][0]["statistics"]['likeCount']
+            self.url_link = "https://www.youtube.com/watch?v=" + self.video_id
+        except Exception:
+            self.video_info = None
+            self.title = None
+            self.number_of_video_views = None
+            self.number_of_likes = None
+            self.url_link = None
 
     def __str__(self):
         """Выводит название видео на Ютуб"""
